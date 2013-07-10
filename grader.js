1ec48c2d1bbcc7a040ecbef78cc8a0b4560788c3
Dmitry <maslov.dmitrij@gmail.com>
2013-07-10 08:26:53 +0000

    modify

--- grader.js
+++ grader.js
@@ -1,6 +1,28 @@
 #!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
 
 var fs = require('fs');
+var util = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
@@ -35,33 +57,39 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var checkUrl = function(url, checksfile) {
-    rest.get(url).on('complete', function(result) {
-        if (result instanceof Error) {
-            sys.puts('Error: ' + result.message);
-        } else {
-            return result;
-        }
-    });
-    return false;
-};
-
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var saveUrlToFile = function(result, response){
+    if (result instanceof Error){
+    console.error('Error: ' + util.format(response.message));
+    } else {
+    var bufferFile = 'buffer.html';
+//  console.error("Wrote %s", 'buffer.html');
+    fs.writeFileSync(bufferFile, result);
+    var checkJson = checkHtmlFile(bufferFile, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+    fs.unlinkSync(bufferFile);
+    }
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url>', 'Path to remote', clone(checkUrl))
+    .option('-u, --url <url>', 'Url to check')
         .parse(process.argv);
+    if (program.url){
+    rest.get(program.url).on('complete', saveUrlToFile);
+    } else if (program.file){
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
-
